8b805a0216eaba4a1f1b9d07d7a0081c38f24611
Anderson <upmalagutti@gmail.com>
2020-06-17 19:33:28 +0200

    Added label and text view using code

--- CodeUI/ViewController.swift
+++ CodeUI/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let LABEL_TEXT = "Name: "
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = LABEL_TEXT
+        view.addSubview(label)
+        
+        let textField = UITextField();
+        textField.borderStyle = .roundedRect
+        view.addSubview(textField)
+        
+        //set label properties
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        label.trailingAnchor.constraint(equalTo: textField.leadingAnchor, constant: 0).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        //set textField properties
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
 
 
